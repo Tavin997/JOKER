24d30565663aca0b2d1f4e224c642fa887fc8ab0
Misael Cardoso Pereira <90993863+1Walke@users.noreply.github.com>
2026-02-15 20:09:27 -0300

    correção

---
+++ Jogo2/Jogo2/Jogador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo2
+{
+    internal class Jogador
+    {
+        private string _nome;
+        private int _fichas;
+        private int _pontos;
+
+        public string Nome { get; set; }
+        public string Fichas { get; set; }
+        public string Pontos { get; set; }
+
+        public void Jogar(Jogador jogador)
+        {
+            Console.WriteLine($"{jogador.Nome} jogou...");
+            Console.ReadKey();
+        }
+
+    }
+}
