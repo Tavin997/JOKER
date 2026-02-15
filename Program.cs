13833ca2f9bce8bca508f266d6279e903b4e1373
Tavin997 <luiz.otavio.loureiro.123@gmail.com>
2026-02-15 02:26:53 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,54 +11,131 @@ namespace Jogo
         static void Main(string[] args)
         {
             Jogador jogador = new Jogador();
+            Bot enemy = new Bot();
             Console.Clear();
-            Console.WriteLine("===== BEM VINDO AO JOKER =====");
-            Console.Write("Digite seu nome: ");
-            jogador.Nome = Console.ReadLine();
-            //Console.WriteLine("Agora escolha o valor da carta Joker (0 - 9): ");
-            //jogador.joker = int.Parse(Console.ReadLine());
-            Joker(jogador);
-        }
-
-        public static void Joker(Jogador jogador)
-        {
-            int escolha;
-            
-            do
+            while(true)
             {
                 Console.Clear();
-                Console.WriteLine("O QUE DESEJA?");
-                Console.WriteLine("1 - JOGAR\n2 - VISUALIZAR TABELA DE PONTOS\n3 - VISUALIZAR TABELA DE COMBINAÇÕES");
-                escolha = int.Parse(Console.ReadLine());
-                Console.Clear();
-                switch (escolha)
+                Console.WriteLine("===== BEM VINDO AO JOKER =====");
+                Console.Write("\nDigite seu nome: ");
+                jogador.Nome = Console.ReadLine();
+                //Console.WriteLine("Agora escolha o valor da carta Joker (0 - 9): ");
+                //jogador.joker = int.Parse(Console.ReadLine());
+                int menu = Menu(jogador);
+                if(menu == 0)
                 {
+                    break;
+                }
+                Mesa(jogador);
+            }
+            
+        }
 
-                    case 2:
-                        Console.WriteLine("1 = 11\n2 = 2\n3 = 3\n4 = 4\n5 = 5\n6 = 0\n7 = 10\n8 = 8\n9 = 10\nJoker = ?");
-                        Console.WriteLine("Pressione qualquer teclar para voltar...");
+        public static int Menu(Jogador jogador)
+        {            
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("╔════════════════════════════════╗");
+                Console.WriteLine("║           MENU PRINCIPAL       ║");
+                Console.WriteLine("╠════════════════════════════════╣");
+                Console.WriteLine("║  1 - JOGAR                     ║");
+                Console.WriteLine("║  2 - TABELA DE PONTOS          ║");
+                Console.WriteLine("║  3 - TABELA DE COMBINAÇÕES     ║");
+                Console.WriteLine("║  4 - SAIR                      ║");
+                Console.WriteLine("╚════════════════════════════════╝");
+                ConsoleKeyInfo tecla = Console.ReadKey(true);
+                Console.Clear();
+                int escolha;
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.D1:
+                        return escolha = 1;
+
+                    case ConsoleKey.D2:
+                        Console.WriteLine("===== TABELA DE PONTOS =====");
+                        Console.WriteLine("1 = 11 pontos");
+                        Console.WriteLine("2 = 2 pontos");
+                        Console.WriteLine("3 = 3 pontos");
+                        Console.WriteLine("4 = 4 pontos");
+                        Console.WriteLine("5 = 5 pontos");
+                        Console.WriteLine("6 = 0 pontos");
+                        Console.WriteLine("7 = 10 pontos");
+                        Console.WriteLine("8 = 8 pontos");
+                        Console.WriteLine("9 = 10 pontos");
+                        Console.WriteLine("Joker = ?");
+                        Console.Write("\nPressione qualquer tecla para voltar...");
                         Console.ReadKey(true);
                         
                         break;
-                    case 1:
-                        jogador.Jogar(jogador);
-                        return;
                         
-                    case 3:
+                    case ConsoleKey.D3:
                         Console.WriteLine("===== COMBINAÇÕES =====");
-                        Console.WriteLine("um par = [SOMA].2\ndois pares = [SOMA]^2.2\numa trinca = [SOMA]^3\nsequência = [SOMA]^5\nfull house = [SOMA]^5.3\nquadra = mude um número da mesa\nqintupla = [SOMA]^5.5");
-                        Console.WriteLine("Pressione qualquer teclar para voltar...");
+                        Console.WriteLine("UM PAR       = [SOMA].2");
+                        Console.WriteLine("DOIS PARES   = [SOMA]^2.2");
+                        Console.WriteLine("UMA TRINCA   = [SOMA]^3");
+                        Console.WriteLine("SEQUÊNCIA    = [SOMA]^5");
+                        Console.WriteLine("FULL HOUSE   = [SOMA]^5.3");
+                        Console.WriteLine("QUADRA       = mude um número da mesa");
+                        Console.WriteLine("QUINTUPLA    = [SOMA]^5.5");
+                        Console.Write("\nPressione qualquer tecla para voltar...");
                         Console.ReadKey(true);
                         break;
+
+                    case ConsoleKey.D4:
+                        return escolha = 0;
+
                     default:
                         Console.WriteLine("Opção Inválida!");
                         Thread.Sleep(200);
                         break;
                 }
             }
-            while (true);
             
 
         }
+
+        public static void Mesa(Jogador jogador)
+        {
+            int rodada = 0;
+            do
+            {
+                Console.Clear();
+                rodada = rodada + 1;
+                Console.WriteLine($"======== {rodada} RODADA =======");
+                Console.WriteLine("======== MESA ========");
+                Random num_mesa = new Random();
+                int[] slot = new int[3];
+                char[] conversao = new char[3];
+
+                Console.Write("||");
+                for(int i = 0; i < 3; i++)
+                {
+                    slot[i] = num_mesa.Next(1, 10);
+                    conversao[i] = slot[i].ToString()[0];
+                    Console.Write($" {conversao[i]} ||");
+                }
+                Thread.Sleep(1000);
+            }while(rodada < 5);
+        }
+
+        //PLANEJO UTILIZAR DESSA FUNÇÃO PARA CONVERTER OS NÚMEROS DA MESA E DAS MÃOS, MAS EU AINDA NEM COMECEI ELA
+        //ISSO SÃO SÓ CÓDIGOS COPIADOS PARA REUTILIZAÇÃO.
+
+        /*public static int Conversao_Num()
+        {
+            char joker = 'J';
+
+            for(int i = 0; i < 3; i++)
+            {
+                slot[i] = num_mesa.Next(1, 10);
+                conversao[i] = slot[i].ToString()[0];
+                if (conversao[i] == '0')
+                {
+                    conversao[i] = (joker);
+                }
+                    Console.Write($" {conversao[i]} ||");
+                }
+        }*/
     }
 }
