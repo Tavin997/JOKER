5a2d63742f24541c447ecc8c4ef6798a48df25a6
Tavin997 <luiz.otavio.loureiro.123@gmail.com>
2026-02-14 21:18:31 -0300

    Update jogador.cs

--- jogador.cs
+++ jogador.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Jogo
+{
+    internal class Jogador
+    {
+        private string _nome;
+        private int _fichas;
+        private int _pontos;
+        public int joker;
+        
+        public string Nome { get; set; }
+        public string Fichas { get; set; }
+        public string Pontos { get; set; }
+
+        public void Jogar(Jogador jogador)
+        {
+            Console.WriteLine($"{jogador.Nome} jogou...");
+            Console.ReadKey();
+            Program.Joker(jogador);
+        }
+
+    }
+}
